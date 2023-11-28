0f5cedd6787f9392509ba8a8c6c67763f2c90c13
eskokado <eskokado@gmail.com>
2023-11-28 06:32:08 -0300

    initial commit

---
+++ src/main/java/com/eskcti/algafoodauthesk/AlgafoodAuthEskApplication.java
@@ -0,0 +1,13 @@
+package com.eskcti.algafoodauthesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodAuthEskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodAuthEskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eskcti/algafoodauthesk/AlgafoodAuthEskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskcti.algafoodauthesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodAuthEskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
