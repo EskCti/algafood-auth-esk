4f029cd275e5df79dde9e9a01f3e48cd81011b48
eskokado <eskokado@gmail.com>
2023-11-29 08:08:27 -0300

    feat: create oauth and check token

--- src/main/java/com/eskcti/algafoodauthesk/AuthorizationServerConfig.java
+++ src/main/java/com/eskcti/algafoodauthesk/AuthorizationServerConfig.java
@@ -1,13 +1,47 @@
 package com.eskcti.algafoodauthesk;
 
-import java.util.Arrays;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+			.inMemory()
+				.withClient("algafood-web")
+				.secret(passwordEncoder.encode("web123"))
+				.authorizedGrantTypes("password")
+				.scopes("write", "read")
+				.accessTokenValiditySeconds(60 * 60 * 6)
+			.and()
+				.withClient("app-mobile")
+				.secret(passwordEncoder.encode("abc555"))
+				.authorizedGrantTypes("outrogranttype", "password")
+				.scopes("write", "read");
+	}
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
 }
