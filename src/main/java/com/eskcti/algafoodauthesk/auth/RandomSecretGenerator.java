cb32e334449ad5ecead0a46e8cfa56c290ddb728
eskokado <eskokado@gmail.com>
2023-12-03 08:42:14 -0300

    feat: create random secret generator

---
+++ src/main/java/com/eskcti/algafoodauthesk/auth/RandomSecretGenerator.java
@@ -0,0 +1,37 @@
+package com.eskcti.algafoodauthesk.auth;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
+public class RandomSecretGenerator {
+    public static void main(String[] args) {
+        int keyLength = 256;
+
+        byte[] secret = generateRandomSecret(keyLength);
+
+        String encodedSecret = Base64.getEncoder().encodeToString(secret);
+
+        System.out.println("Encoded Secret (256 bits): " + encodedSecret);
+    }
+
+    private static byte[] generateRandomSecret(int keyLength) {
+        try {
+            SecureRandom secureRandom = new SecureRandom();
+            byte[] salt = new byte[keyLength / 8];
+            secureRandom.nextBytes(salt);
+
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            PBEKeySpec keySpec = new PBEKeySpec("algaworks".toCharArray(), salt, 65536, keyLength);
+            SecretKey secretKey = secretKeyFactory.generateSecret(keySpec);
+
+            return secretKey.getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException("Algoritmo de criptografia não suportado", e);
+        }
+    }
+}
