da8e25f0977a17b1640f3b2c7adf12c9abb6dc6a
eskokado <eskokado@gmail.com>
2023-12-03 15:30:10 -0300

    feat: create KeyStoreConfig

---
+++ src/main/java/com/eskcti/algafoodauthesk/auth/KeyStoreConfig.java
@@ -0,0 +1,28 @@
+package com.eskcti.algafoodauthesk.auth;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class KeyStoreConfig {
+
+    @Autowired
+    private KeyStoreProperties properties;
+
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure().directory("src/.env").load();
+    }
+
+    @Bean
+    public boolean configPropertiesWithEnv(Dotenv dotenv) {
+        properties.setKeypairAlias(dotenv.get("JWT_KEYSTORE_KEYPAIR_ALIAS"));
+        properties.setPath(dotenv.get("JWT_KEYSTORE_PATH"));
+        properties.setPassword(dotenv.get("JWT_KEYSTORE_PASSWORD"));
+        return true;
+    }
+}
