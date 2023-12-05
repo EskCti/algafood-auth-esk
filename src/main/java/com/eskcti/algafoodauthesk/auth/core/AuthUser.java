20dd74990dd5af365004cf1b1aea681f1a32328a
eskokado <eskokado@gmail.com>
2023-12-05 06:02:55 -0300

    feat: add property userId into AuthUser

--- src/main/java/com/eskcti/algafoodauthesk/auth/core/AuthUser.java
+++ src/main/java/com/eskcti/algafoodauthesk/auth/core/AuthUser.java
@@ -9,10 +9,11 @@ import java.util.Collections;
 public class AuthUser extends User {
     private static final long serialVersionUID = 1L;
 
+    private Long userId;
     private String fullName;
     public AuthUser(com.eskcti.algafoodauthesk.auth.domain.User user) {
         super(user.getEmail(), user.getPassword(), Collections.emptyList());
-
+        this.userId = user.getId();
         this.fullName = user.getName();
     }
 }
